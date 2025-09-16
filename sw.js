d8209a0094b3fa2e5377529802f45ecaced62f11
JS <jobbe.smits@gmail.com>
2025-09-16 16:24:00 +0200

    0.0.3 - action icon - check() logic: add/subtract mistake - launchQueue - launch-clients

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "0.0.2";
+const VERSION = "0.0.3";
 const CACHE_NAME = `quiz-cache-${VERSION}`;
 const APP_STATIC_RES = [
   "/notQuizlet/",
@@ -49,7 +49,7 @@ self.addEventListener('activate', function(event) {
           return caches.delete(key);
         }
       }));
-    }) 
+    })
   );
 });
 
